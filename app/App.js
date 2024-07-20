17ac79577d5553c488d4c06f4e100c077b0808f5
Savindi20 <savindidadallage@gmail.com>
2024-07-20 20:31:54 +0530

    home screen and index js added

--- app/App.js
+++ app/App.js
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { HomeScreen } from './screens';
+
+const Stack = createNativeStackNavigator();
+
 const app = () => {
   return (
-    <View>
-      <Text>app</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
